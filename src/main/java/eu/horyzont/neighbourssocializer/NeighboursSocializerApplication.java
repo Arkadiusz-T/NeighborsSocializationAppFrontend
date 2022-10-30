083a9b843f301f86a875d82fb0f686eca7ec745e
Endorocket <43682886+Endorocket@users.noreply.github.com>
2022-10-30 14:47:39 +0100

    first commit

---
+++ src/main/java/eu/horyzont/neighbourssocializer/NeighboursSocializerApplication.java
@@ -0,0 +1,13 @@
+package eu.horyzont.neighbourssocializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighboursSocializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighboursSocializerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/horyzont/neighbourssocializer/NeighboursSocializerApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.horyzont.neighbourssocializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighboursSocializerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
